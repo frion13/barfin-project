82d7048836b392384a09810b93c862f971e0e3f2
John Doe <frio_13@bk.ru>
2024-04-23 15:30:31 +0300

    change

--- src/test/java/config/DriverConfig.java
+++ src/test/java/config/DriverConfig.java
@@ -6,7 +6,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/${driver}.properties"
+        "classpath:config/remote.properties"
 })
 public interface DriverConfig extends Config {
     @Key("browser.name")
